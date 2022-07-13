af90a201b25588db08c66ec127fdb386ad529ac8
Pranat1 <74774569+Pranat1@users.noreply.github.com>
2022-07-13 20:10:49 +0530

    Revert "TankTypes"
    This reverts commit 74a4e9aa66f9b74fabc147c02f46fd2cf4711534.

--- Assets/Scripts/ScriptableObjects/BulletListScriptableObject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "BulletListScriptableObject", menuName = "ScriptableObjectList/ListBulletTask")]
-public class BulletListScriptableObject : ScriptableObject
-{
-    public BulletScriptableObject[] bullets;
-}
--- Assets/Scripts/ScriptableObjects/EnemyListScriptableObject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "EnemyListScriptableObject", menuName = "ScriptableObjectList/ListEnemyTask")]
-public class EnemyListScriptableObject : ScriptableObject
-{
-    public EnemyScriptableObject[] tanks;
-}
